fd9d83fb153ece9de021906c7752f3ce9ddd5673
Kento Kawakami <kawakamikento@gmail.com>
2020-07-10 23:23:04 -0400

    Set up backend for user log in

---
+++ Controllers/SessionsController.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using _09_Suncoast_Overflow.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace _09_Suncoast_Overflow.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        readonly protected string JWT_KEY;
+
+        public SessionsController(DatabaseContext context, IConfiguration config)
+        {
+            _context = context;
+            JWT_KEY = config["JWT_KEY"];
+        }
+
+        public class LoginUser
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginUser loginUser)
+        {
+            var foundUser = await _context.Users.FirstOrDefaultAsync(user => user.Email == loginUser.Email);
+
+            if (foundUser != null && foundUser.IsValidPassword(loginUser.Password))
+            {
+                var response = new
+                {
+                    token = new TokenGenerator(JWT_KEY).TokenFor(foundUser),
+                    user = foundUser
+                };
+
+                return Ok(response);
+            }
+            else
+            {
+                var response = new
+                {
+                    status = 400,
+                    errors = new List<string>() { $"User does not exist" }
+                };
+
+                return BadRequest(response);
+            }
+        }
+    }
+}
