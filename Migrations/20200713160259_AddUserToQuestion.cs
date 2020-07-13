67553f7747a73b8bc436aac3cb06b80552acf47a
Kento Kawakami <kawakamikento@gmail.com>
2020-07-13 12:04:02 -0400

    Fixed UsersId migrations

---
+++ Migrations/20200713160259_AddUserToQuestion.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _09Suncoast_Overflow.Migrations
+{
+    public partial class AddUserToQuestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Questions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Questions");
+        }
+    }
+}
